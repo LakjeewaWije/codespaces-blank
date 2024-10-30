6f2b5edb2b6cfdf779c92c4c00fb21c62d611bb7
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-30 22:55:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { createServer } = require('http');
+
+const hostname = '0.0.0.0'; // Listen on all available interfaces
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin'); // Set Referrer-Policy header
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
