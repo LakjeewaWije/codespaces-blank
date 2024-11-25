30778bfe37ca3d94d785de7c6cf7b00895d11959
LakjeewaWije <layerswije@gmail.com>
2024-11-25 14:44:16 +0000

    Changed : default route text

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin'); // Set Referrer-Policy header
-  res.end('Hi this a sample node application to demonstrate the network CA One!');
+  res.end('Hi this a sample node application to demonstrate the network CA One by four group members!');
 });
 
 server.listen(port, hostname, () => {
